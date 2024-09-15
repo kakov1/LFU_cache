82b702b2f9b3b52ae80770168c5b2fcd942c9b4d
kakov <semendud74@gmail.com>
2024-09-16 01:51:08 +0300

    improved tests

--- Tests/test_generate.py
+++ Tests/test_generate.py
@@ -1,10 +1,13 @@
 import random
 
-
-for i in range(1, 11):
+cache_capacity = int(input("Введите размер кэша\n"))
+pages_number = int(input("Введите число страниц\n"))
+tests_number = int(input("Введите число тестов\n"))
+for i in range(1, tests_number + 1):
     file = open(f"{i}test.txt", 'w')
 
-    file.write("10000 100000 ")
-    for i in range(100000):
+    file.write(f"{cache_capacity} {pages_number} ")
+    for i in range(pages_number):
         file.write(f"{random.randint(1, 100000)} ")
+print("Тесты сгенерированы")
         
